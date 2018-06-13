078f4ae6cee4784ce07f5ad9c589d9953946c2cf
Gilbert Keen <gilbert.keen@gmail.com>
2018-06-13 15:38:35 +0300

    add the requisite variables

--- designs.js
+++ designs.js
@@ -1,5 +1,17 @@
 // Select color input
+var colorInput = $("#color-picker");
 // Select size input
+var createGridBtn = $("#create-grid");
+// Select Reset button
+var resetBtn = ("#reset");
+var clearBtn = $("#clear-grid");
+// Select height and width input
+ var inputRows = $("#input-rows");
+ var inputColumns = $("#input-columns");
+ // Table
+ var row = '<tr></tr>';
+ var column = '<td></td>';
+ 
 
 // When size is submitted by the user, call makeGrid()
 
