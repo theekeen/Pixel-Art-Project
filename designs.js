1cb84d9bd45b428e90ecaaa73a8f9fe8314fba23
Gilbert Keen <gilbert.keen@gmail.com>
2018-06-30 16:22:34 +0300

    define variables for the user interacting with

--- designs.js
+++ designs.js
@@ -1,17 +1,42 @@
-// Select color input
-var colorInput = $("#color-picker");
-
 // Select size input
-var createGridBtn = $("#create-grid");
+const createGridBtn = $('#create-grid');
 // Select Reset button
-var resetBtn = ("#reset");
-var clearBtn = $("#clear-grid");
+const resetBtn = $('#reset');
+// Select Clear Grid button
+const clearBtn = $('#clear-grid')
 // Select height and width input
- var inputRows = $("#input-rows");
- var inputColumns = $("#input-columns");
- // Table
- var row = '<tr></tr>';
- var column = '<td></td>';
- var pixelCanvas = $('#pixel-canvas');
+const inputRows = $('#input-rows');
+const inputColumns = $('#input-columns');
+// Select color input
+const colorInput = $('#color-picker');
+// Select table
+const pixelCanvas = $('#pixel-canvas');
+// Rows and columns
+const row = '<tr></tr>';
+const column = '<td></td>';
+
+// RESET page
+function reset(){
+    removeGrid();
+    colorInput.val('#4286f4');
+    selectedColor = colorInput.val();
+    inputRows.val(10);
+    inputColumns.val(10);
+    currentGridRows = 0;
+    inputRows.css('color', '');
+    inputColumns.css('color', '');
+}
+
+resetBtn.click(reset);
+
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+    // Your code goes here!
+    
+    }
+    
 
 
